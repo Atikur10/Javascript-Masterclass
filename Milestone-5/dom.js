f3f6c09bd95e968adaf42a9de9becdc5e22d5001
Badhon Ahmed <shohana2890@gmail.com>
2023-02-11 03:38:37 +0600

    added all file

---
+++ Milestone-5/dom.js
@@ -0,0 +1,59 @@
+const liCollection = document.getElementsByTagName('li');
+const allHeading = document.getElementsByTagName('h1');
+console.log(liCollection);
+for (const li of liCollection) {
+    console.log(li.firstChild.nodeType)
+}
+for (const heading of allHeading) {
+    console.log(heading.innerText)
+}
+
+let title = document.getElementById('visit-title');
+title.innerHTML = 'Which Place I like to visit';
+title.style.fontSize = '50px'
+console.log(title)
+let language = document.querySelector('h1.language');
+language.style.color = 'black';
+language.style.fontSize = '2rem';
+language.style.border = '1px solid red';
+language.style.textAlign = 'center'
+console.log(language)
+let favPlace = document.getElementsByClassName('fav-place');
+for (const x of favPlace) {
+    console.log(x)
+}
+console.log(favPlace)
+
+const headerTitle = document.querySelector('.header-title');
+headerTitle.setAttribute('title','World Health Organizations')
+headerTitle.style.borderBottom = '1px solid red';
+headerTitle.style.textAlign = 'center';
+headerTitle.style.marginBottom = '10px'
+console.log(headerTitle)
+
+
+let getElement = document.querySelectorAll('.get-element li');
+for (const li of getElement) {
+    console.log(li)
+}
+console.log(getElement);
+
+
+getElement.forEach(function(num){
+    console.log(num)
+})
+
+let getElement1 = document.getElementById('get-element');
+getElement1.classList;
+getElement1.classList.remove('abc');
+getElement1.classList.add('title-bg');
+console.log(getElement1)
+
+
+
+
+
+
+
+
+
